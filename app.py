4321d9b469c1adc327e1baa32a1b68bc3b63d86f
ThyMakra <45727563+ThyMakra@users.noreply.github.com>
2019-06-13 14:36:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,53 @@
 from flask import Flask,request
 from pymessenger.bot import Bot
-import random
+import random, re, os, requests
+
+#paste your messenger app page access token here
+PAGE_ACCESS_TOKEN = 'EAAEXm0CJeicBAOwLUXFCsejHYcJGC4isNkNTzG9sB2uMDNZCJ8rOuNhXKaBqw5ZALYmvulIHyirDWxcYZAXpY0xu4zgTdYZC0sIe4JLXX4ftWmLWCAwUuOMZC26GNV3AFn1FzGZAQyzCznz8rTNrow5JpljDoCRrJE2UrHNOfbHky5GYoRm9ZCs'
+#
+VERIFY_TOKEN = 'secret'
+
 app = Flask(__name__)
-bot = Bot('Your_token')
+bot = Bot(PAGE_ACCESS_TOKEN)
+
+#create folder and save file if is a file or image
+def save_attachment(message):
+    file_extension = ['.ppt','.pps','.pptx','.wpd','.wps','.wks','.tex','.rtf','.odt','.txt','.pdf','.csv','.xml','.py','.html','.js','.css','.ods','.xlr','.xls','.xlsx','.docx','.doc']
+    image_extension = ['.jpg','.png','.gif','.ai','.jpeg','.svg','.bmp']
+    # message['message']
+    msg_type = message['attachments'][0]['type'] 
+    msg_url = message['attachments'][0]['payload']['url']
+    if msg_type == 'file':    
+        for extn in file_extension:
+            if extn+'?' in msg_url:
+                fname = re.search(r'/[^./?]+'+extn+'[?]',msg_url)
+                # print(fname.group())
+                name = fname.group()[1:-1]            
+                fpath = os.path.join('.','File',extn[1:].upper())
+                if os.path.isdir(fpath) == False:
+                    os.makedirs(fpath)
+                openobj = open(os.path.join(fpath,name), 'wb')
+                openobj.write(requests.get(msg_url).content)
+                openobj.close()
+                return f'{extn} file has been saved'    
+        return "File extension not recognized"
+    elif msg_type == 'image':    
+        for extn in image_extension:        
+            if extn+'?' in msg_url:
+                print(extn)
+                print(200)
+                fname = re.search(r'/[^./?]+'+extn+'[?]',msg_url)            
+                name = fname.group()[1:-1]            
+                fpath = os.path.join('.','Image',extn[1:].upper())
+                if os.path.isdir(fpath) == False:
+                    os.makedirs(fpath)
+                openobj = open(os.path.join(fpath,name), 'wb')
+                openobj.write(requests.get(msg_url).content)
+                openobj.close()
+                return "Image saved"
+        return "Image extension unrecognized"
+    else :
+        return "Attachment unrecognized"
 
 @app.route('/',methods=["GET","POST"])
 def webhook():
@@ -12,26 +57,28 @@ def webhook():
 
     else:
         data = request.get_json()
-        print(data)
+        # print(data)
         for event in data['entry']:
             messaging = event['messaging']
             for message in messaging:
+                # check if we get the message or not
                 if message.get('message'):
                 #Facebook Messenger ID for user so we know where to send response back to
                     recipient_id = message['sender']['id']
 
                     if message['message'].get('text'):
+                        # print(message['message'].get('text')) #the message the sender sent
                         response_sent_text = get_message()
                         send_message(recipient_id, response_sent_text)
 
                     #if user sends us a GIF, photo,video, or any other non-text item
-                    if message['message'].get('attachments'):
-                        response_sent_nontext = get_message()
+                    elif message['message'].get('attachments'):                        
+                        response_sent_nontext = save_attachment(message['message'])
                         send_message(recipient_id, response_sent_nontext)
         return 'message procced'
 
 def verify_fb_token(token_sent):
-    if token_sent == "idontknow":
+    if token_sent == VERIFY_TOKEN:
         return request.args.get("hub.challenge")
     return "Invalid verifiation token"
 
